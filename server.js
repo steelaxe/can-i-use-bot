018f6b603df0fead805a63331e0fe0d0d35b08c6
TakenoriOkada <okajax@gmail.com>
2016-05-29 13:15:53 +0900

    アタッチメント再test

--- server.js
+++ server.js
@@ -112,29 +112,31 @@ bot.add('/', [
         if (query == "test") {
 
             var msg = new builder.Message().addAttachment({
-
-                "fallback": "Required plain-text summary of the attachment.",
-                "color": "#36a64f",
-                "pretext": "Optional text that appears above the attachment block",
-                "author_name": "Bobby Tables",
-                "author_link": "http://flickr.com/bobby/",
-                "author_icon": "http://flickr.com/icons/bobby.jpg",
-                "title": "Slack API Documentation",
-                "title_link": "https://api.slack.com/",
-                "text": "Optional text that appears within the attachment",
-                "fields": [
+                "text": "I am a test message http://slack.com",
+                "attachments": [
                     {
-                        "title": "Priority",
-                        "value": "High",
-                        "short": false
+                        "fallback": "Required plain-text summary of the attachment.",
+                        "color": "#36a64f",
+                        "pretext": "Optional text that appears above the attachment block",
+                        "author_name": "Bobby Tables",
+                        "author_link": "http://flickr.com/bobby/",
+                        "author_icon": "http://flickr.com/icons/bobby.jpg",
+                        "title": "Slack API Documentation",
+                        "title_link": "https://api.slack.com/",
+                        "text": "Optional text that appears within the attachment",
+                        "fields": [
+                            {
+                                "title": "Priority",
+                                "value": "High",
+                                "short": false
                     }
                 ],
-                "image_url": "http://my-website.com/path/to/image.jpg",
-                "thumb_url": "http://example.com/path/to/thumb.png",
-                "footer": "Slack API",
-                "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-                "ts": 123456789
-
+                        "image_url": "http://my-website.com/path/to/image.jpg",
+                        "thumb_url": "http://example.com/path/to/thumb.png",
+                        "footer": "Slack API",
+                        "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+                        "ts": 123456789
+                    }]
             });
             session.send(msg);
 
